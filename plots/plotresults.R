8849957b1d327790023e758b9f2b527e2482a770
Hannes Muehleisen <hannes@muehleisen.org>
2018-03-20 13:16:31 +0100

    update

--- plots/plotresults.R
+++ plots/plotresults.R
@@ -22,7 +22,7 @@ abplot <- function(fa, fb, la, lb, out) {
 }
 
 abplot("monetdb.default.debug.sf1-q1.tsv", "monetdb.default.optimal.sf1-q1.tsv", "MonetDB", "MonetDB*", "compilation-flags.pdf")
-abplot("hyper-sf1-q1.tsv", "hand-sf1-q1.tsv", "HyPer", "'TimDB'", "hand-rolled.pdf")
+abplot("monetdb.default.optimal.sf1-q1.tsv", "hand-sf1-q1.tsv", "MonetDB", "'TimDB'", "hand-rolled.pdf")
 abplot("mysql-sf1-q1.tsv", "postgres.optimal.sf1-q1.tsv", "MariaDB", "Postgres", "postgres-better-mysql.pdf")
 abplot("postgres.default.sf1-q9.tsv", "postgres.optimal.sf1-q9.tsv", "Postgres", "Postgres*", "postgres-config.pdf")
 abplot("sqlite-sf1-q9-hard-joinorder", "sqlite-sf1-q9-defaullt-joinorder", "SQLite", "SQLite*", "sqlite-plan.pdf")
@@ -33,41 +33,30 @@ aggr <- function(f, sys) {
 	a <- read.table(sprintf("../results/%s", f))$V1
 	data.frame(time_sec=median(a), sys=sys, loconf= sort(a)[2], hiconf=sort(a)[9])
 }
-	
-
 
 	
-	my <- aggr("mysql-sf1-q1.tsv", "MySQL")
-	my$grp <- 1
+my <- aggr("mysql-sf1-q1.tsv", "MySQL")
+my$grp <- 1
 
-	pg <- aggr("postgres.optimal.sf1-q1.tsv", "Postgres")
-	pg$grp <- 1
+pg <- aggr("postgres.optimal.sf1-q1.tsv", "Postgres")
+pg$grp <- 1
 
-	pg2 <- pg
-	pg2$grp <- 2
+pg2 <- pg
+pg2$grp <- 2
 
-	sl <- aggr("sqllite.disk.sf1-q1.tsv", "SQLite")
-	sl$grp <- 2
+sl <- aggr("sqllite.disk.sf1-q1.tsv", "SQLite")
+sl$grp <- 2
 
-	sl2 <- sl
-	sl2$grp <- 3
+sl2 <- sl
+sl2$grp <- 3
 
-	mf <- aggr("mysql-floats-sf1-q1.tsv", "MySQL")
-	mf$grp <- 3
+mf <- aggr("mysql-floats-sf1-q1.tsv", "MySQL*")
+mf$grp <- 3
 
-	df <- rbind(my, pg, pg2, sl, sl2, mf)
-	df$grp <- as.factor(df$grp)
+df <- rbind(my, pg, pg2, sl, sl2, mf)
+df$grp <- as.factor(df$grp)
 
+pdf("all.pdf", height=5, width=12)
+print(ggplot(df, aes(x=sys, y=time_sec, group=sys)) + geom_bar(stat="identity", position = position_dodge(), width=.35, fill = "#777777") + xlab("") + ylab("Median time (s)") + theme  + geom_errorbar(aes(ymin=loconf, ymax=hiconf), width=.1, size=1.2) + geom_text(aes(label=round(time_sec, 2)), vjust=-.5, hjust=-.5, family="serif", size=7) + scale_y_continuous(limits=c(0, 14)) + facet_grid(~grp, scales="free") + theme(strip.text.x = element_blank()))
 
-
-	pdf("all.pdf", height=5, width=12)
-	print(ggplot(df, aes(x=sys, y=time_sec, group=sys)) + geom_bar(stat="identity", position = position_dodge(), width=.35, fill = "#777777") + xlab("") + ylab("Median time (s)") + theme  + geom_errorbar(aes(ymin=loconf, ymax=hiconf), width=.1, size=1.2) + geom_text(aes(label=round(time_sec, 2)), vjust=-.5, hjust=-.5, family="serif", size=7) + facet_grid(~grp, scales="free") + theme(  strip.text.x = element_blank()))
-
-	dev.off() 
-# abplot("sqlite-sf1-q9-hard-joinorder", "sqlite-sf1-q9-defaullt-joinorder", "SQLite (a)", "SQLite (b)", "sqlite-plan.pdf")
-# mysql float 4.5
-# sqlite 8.2
-# postgres 9.7
-# mysql 12.2
-
-# results are the same within tpch accuracy spec
+dev.off() 
